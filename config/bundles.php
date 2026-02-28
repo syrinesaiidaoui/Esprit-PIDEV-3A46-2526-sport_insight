b21a109df00f04fc98ed16bf0647f88567fb7ee9
sayda180 <guenichisayda30@gmail.com>
2026-02-28 04:35:15 +0100

    Final cleanup and fixes for sayda-annonces branch

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,7 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
 ];
 
